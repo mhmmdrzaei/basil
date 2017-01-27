fe216a05ea92d4e12b1c888ac5c277e778d30a95
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-01-27 08:03:26 -0500

    added styling to projects page

--- page-projects.php
+++ page-projects.php
@@ -26,30 +26,30 @@
           <?php  dynamic_sidebar( 'projects-widget-area' ); ?>
       
     </div>
-  
-    <?php $dreamBeanQuery = new WP_Query(array(
-            // 'post_per_page' => 1,// if you want all its -1
-            'post_type' => array( 'projects' ),
-            'post_per_page' => array(-1)
-        )); ?> 
-  
-  
-    <?php if($dreamBeanQuery-> have_posts()):?>
-    <?php while($dreamBeanQuery->have_posts()): ?> 
-    <?php $dreamBeanQuery->the_post(); ?>
-    <?php $dreamBeanQuery->the_content(); ?> 
-    
-  
-      <div class="projectpost">
-        <?php the_post_thumbnail( 'medium' ) ?>
-        <p><a href="<?php the_permalink(); ?>">
-                  <?php the_title(); ?></a></p>
-      </div>
-  
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?> 
-                <!-- this will end the and reset and go back to normal so you can go back to normal to your page -->
-      <?php endif; ?>
+ <div class="projectsMain"> 
+       <?php $dreamBeanQuery = new WP_Query(array(
+               // 'post_per_page' => 1,// if you want all its -1
+               'post_type' => array( 'projects' ),
+               'post_per_page' => array(-1)
+           )); ?> 
+     
+     
+       <?php if($dreamBeanQuery-> have_posts()):?>
+       <?php while($dreamBeanQuery->have_posts()): ?> 
+       <?php $dreamBeanQuery->the_post(); ?>
+       <?php $dreamBeanQuery->the_content(); ?> 
+       
+     
+         <div class="projectpost">
+           <?php the_post_thumbnail( 'medium' ) ?>
+           <p><a href="<?php the_permalink(); ?>">
+                     <?php the_title(); ?></a></p>
+         </div>
+     
+         <?php endwhile; ?>
+         <?php wp_reset_postdata(); ?> 
+                   <!-- this will end the and reset and go back to normal so you can go back to normal to your page -->
+         <?php endif; ?></div>
   
       </div> <!-- /.content --></div>
 
