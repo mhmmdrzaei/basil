b87979007600d17c41ac8410275512860f5ef813
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2018-09-03 21:44:47 -0400

    heading changes

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
-  <link href="https://fonts.googleapis.com/css?family=Nunito:400,700|Permanent+Marker" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:400,700|Baloo+Da" rel="stylesheet">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <!-- stylesheets should be enqueued in functions.php -->
   <?php wp_head(); ?>
